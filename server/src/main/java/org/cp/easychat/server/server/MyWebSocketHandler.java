c6b6b730d0f49221717b8215a331811554b6d2d6
chengpeng <chengpengyeah@126.com>
2022-11-04 09:52:52 +0800

    echo server

--- server/src/main/java/org/cp/easychat/server/server/MyWebSocketHandler.java
+++ server/src/main/java/org/cp/easychat/server/server/MyWebSocketHandler.java
@@ -2,6 +2,7 @@ package org.cp.easychat.server.server;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,11 +15,19 @@ public class MyWebSocketHandler extends SimpleChannelInboundHandler<Object> {
 
 	@Override
 	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-		log.info("️ connection close from {}", ctx.channel().remoteAddress());
+		log.info(" ⬇️️ up connection close from {}", ctx.channel().remoteAddress());
 	}
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-		log.info(" New Message: {}, from {}", msg, ctx.channel().remoteAddress());
+		log.info(" 🆕 New Message: {}, from {}", msg, ctx.channel().remoteAddress());
+		if (! (msg instanceof TextWebSocketFrame)) {
+			log.error("message is not text, {}", msg);
+			return;
+		}
+		TextWebSocketFrame request = (TextWebSocketFrame) msg;
+		log.info("received text message : {}", request);
+
+		ctx.writeAndFlush(new TextWebSocketFrame("server send :" + request.text()));
 	}
 }
